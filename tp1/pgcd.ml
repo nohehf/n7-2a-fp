b59e14982b5b4dc4aac0808ef6e2b4386d1ceb07
nohehf <nohe.hinniger.foray@gmail.com>
2024-02-14 08:49:30 +0100

    feat: naive pgcd

--- tp1/pgcd.ml
+++ tp1/pgcd.ml
@@ -1,10 +1,25 @@
 (* Exercice à rendre *)
-(*  
-  pgcd: int -> int -> int
-  calcule le pgcd de a et b
-  parametres: a et b, entiers naturels
-  precodition: a et b strictement positifs
+(*
+   pgcd: int -> int -> int
+   Calcule le pgcd de a et b
+   Parametres: a et b, entiers naturels
+   Precodition: a et b strictement positifs
 *)
-(* let pgcd a b = if a = 1 || b = 1 then 1 else pgcd mod() *)
-(*  TO DO : tests unitaires *)
- 
+let rec pgcd a b =
+  if a = b then a else if a > b then pgcd (a - b) b else pgcd a (b - a)
+
+let%test _ = pgcd 1 1 = 1
+let%test _ = pgcd 1 2 = 1
+let%test _ = pgcd 2 1 = 1
+let%test _ = pgcd 15 5 = 5
+let%test _ = pgcd 5 15 = 5
+let%test _ = pgcd 126 54 = 18
+let%test _ = pgcd 54 126 = 18
+
+(* primes *)
+let%test _ = pgcd 3 7 = 1
+let%test _ = pgcd 7 3 = 1
+
+(* relatively primes *)
+let%test _ = pgcd 20 9 = 1
+let%test _ = pgcd 9 20 = 1
