0c5b3a94e6c9f2d59970a9c4b807c7a4c96ef90e
nohehf <nohe.hinniger.foray@gmail.com>
2024-02-23 14:27:07 +0100

    feat: setup tp2

---
+++ tp2/custom_tp2.ml
@@ -0,0 +1,9 @@
+(* 
+  Exercice 1
+  Inserts e in a sorted list using the comparaison function ord_func
+  Inputs:
+    - ord_func: 'a '-> 'a '-> bool; ordering function.
+    - e: 'a; element to insert
+    - sorted_list: 'a list; the sorted list in which e should be inserted
+*)
+(* let insert ord_func e sorted_list = List.fold_right fun h q -> if ord_func h e  *)
